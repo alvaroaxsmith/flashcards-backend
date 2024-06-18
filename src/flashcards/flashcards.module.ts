dba96efc7f162f91a4372a4328bb4bd3fd90d242
Álvaro Machado Ferreira <alvaromachadoferreira@hotmail.com>
2024-06-18 02:33:22 +0000

    feat: add flashcards service

--- src/flashcards/flashcards.module.ts
+++ src/flashcards/flashcards.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { FlashcardsService } from './flashcards.service';
 
-@Module({})
+@Module({
+  providers: [FlashcardsService]
+})
 export class FlashcardsModule {}

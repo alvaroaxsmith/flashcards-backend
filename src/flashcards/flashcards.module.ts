538289e18ef9d4ae111b823e21581c1b46975d96
Álvaro Machado Ferreira <alvaromachadoferreira@hotmail.com>
2024-06-18 03:39:51 +0000

    feat: update flashcards module

--- src/flashcards/flashcards.module.ts
+++ src/flashcards/flashcards.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FlashcardsService } from './flashcards.service';
 import { FlashcardsController } from './flashcards.controller';
+import { Flashcard } from '../entities/flashcard.entity';
+import { Deck } from '../entities/deck.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Flashcard, Deck])],
   providers: [FlashcardsService],
-  controllers: [FlashcardsController]
+  controllers: [FlashcardsController],
 })
 export class FlashcardsModule {}

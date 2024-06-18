7b06af1aa4134335bfb8d20037a22b2021a409c2
Álvaro Machado Ferreira <alvaromachadoferreira@hotmail.com>
2024-06-18 02:37:44 +0000

    feat: update flashcards service

--- src/flashcards/flashcards.service.ts
+++ src/flashcards/flashcards.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FlashcardsRepository } from './repositories/flashcards.repository';
+import { Flashcard } from '../entities/flashcard.entity';
 
 @Injectable()
-export class FlashcardsService {}
+export class FlashcardsService {
+  constructor(
+    @InjectRepository(FlashcardsRepository)
+    private flashcardsRepository: FlashcardsRepository,
+  ) {}
+
+  findAll(): Promise<Flashcard[]> {
+    return this.flashcardsRepository.findAllFlashcards();
+  }
+
+  create(flashcard: Flashcard): Promise<Flashcard> {
+    return this.flashcardsRepository.createFlashcard(flashcard);
+  }
+
+  update(id: number, flashcard: Partial<Flashcard>): Promise<Flashcard> {
+    return this.flashcardsRepository.updateFlashcard(id, flashcard);
+  }
+}

d665c98f0f02deb6da520d457d717d4ec94b1e89
Álvaro Machado Ferreira <alvaromachadoferreira@hotmail.com>
2024-06-18 03:38:05 +0000

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { FlashcardsModule } from './flashcards/flashcards.module';
+import { Flashcard } from './entities/flashcard.entity';
+import { Deck } from './entities/deck.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna as configurações disponíveis globalmente
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [Flashcard, Deck],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     FlashcardsModule,
   ],

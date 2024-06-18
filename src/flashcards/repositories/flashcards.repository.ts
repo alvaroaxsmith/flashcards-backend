25ebd829710af9f02ba102a86f1816ebbf84d0d7
Álvaro Machado Ferreira <alvaromachadoferreira@hotmail.com>
2024-06-18 03:39:26 +0000

    feat: update flashcards repository

--- src/flashcards/repositories/flashcards.repository.ts
+++ src/flashcards/repositories/flashcards.repository.ts
@@ -1,17 +1,16 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Flashcard } from '../../entities/flashcard.entity';
 
-@EntityRepository(Flashcard)
 export class FlashcardsRepository extends Repository<Flashcard> {
-  findAllFlashcards(): Promise<Flashcard[]> {
+  async findAllFlashcards(): Promise<Flashcard[]> {
     return this.find();
   }
 
-  createFlashcard(flashcard: Flashcard): Promise<Flashcard> {
+  async createFlashcard(flashcard: Flashcard): Promise<Flashcard> {
     return this.save(flashcard);
   }
 
-  updateFlashcard(id: number, flashcard: Partial<Flashcard>): Promise<Flashcard> {
+  async updateFlashcard(id: number, flashcard: Partial<Flashcard>): Promise<Flashcard> {
     return this.save({ ...flashcard, id });
   }
 }

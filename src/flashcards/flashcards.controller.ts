67dfcd4352c647826d2d21e4824bb07bab2b8400
Álvaro Machado Ferreira <alvaromachadoferreira@hotmail.com>
2024-06-18 02:38:49 +0000

    feat: update flashcards controller

--- src/flashcards/flashcards.controller.ts
+++ src/flashcards/flashcards.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param } from '@nestjs/common';
+import { FlashcardsService } from './flashcards.service';
+import { Flashcard } from '../entities/flashcard.entity';
 
 @Controller('flashcards')
-export class FlashcardsController {}
+export class FlashcardsController {
+  constructor(private readonly flashcardsService: FlashcardsService) {}
+
+  @Get()
+  findAll(): Promise<Flashcard[]> {
+    return this.flashcardsService.findAll();
+  }
+
+  @Post()
+  create(@Body() flashcard: Flashcard): Promise<Flashcard> {
+    return this.flashcardsService.create(flashcard);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() flashcard: Partial<Flashcard>): Promise<Flashcard> {
+    return this.flashcardsService.update(Number(id), flashcard);
+  }
+}
